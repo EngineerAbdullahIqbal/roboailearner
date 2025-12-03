fb84125816f682563eba2228255f78d4c8efb07f
Engineer Abdullah Iqbal <hassangb544@gmail.com>
2025-12-03 13:04:24 +0500

    Update

--- robotics_book_content/docusaurus.config.ts
+++ robotics_book_content/docusaurus.config.ts
@@ -22,7 +22,7 @@ const config: Config = {
   url: 'https://engineerabdullahiqbal.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-    baseUrl: '/',
+    baseUrl: '/roboailearner/',
 
   // GitHub pages deployment config.
   organizationName: 'EngineerAbdullahIqbal', // Usually your GitHub org/user name.
